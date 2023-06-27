56413a3840dd2d53715a6fc8795706a4d093f7b7
Bloodov Pavel <kasper6024@gmail.com>
2023-06-28 01:20:02 +0300

    Setup media files serving

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,8 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from where_to_go import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('places.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
