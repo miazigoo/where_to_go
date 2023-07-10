1622c1c98f44cccd95c529fda69137edc51084f8
Bloodov Pavel <kasper6024@gmail.com>
2023-07-10 19:01:45 +0300

    Убрана лишняя функциональность

--- places/models.py
+++ places/models.py
@@ -3,19 +3,7 @@ from tinymce.models import HTMLField
 from django.utils.safestring import mark_safe
 
 from where_to_go import settings
-from uuid import uuid4
 from pytils.translit import slugify
-
-
-def unique_slugify(instance, slug):
-    """
-    Генератор уникальных SLUG для моделей, в случае существования такого SLUG.
-    """
-    model = instance.__class__
-    unique_slug = slugify(slug)
-    while model.objects.filter(slug=unique_slug).exists():
-        unique_slug = f'{unique_slug}-{uuid4().hex[:8]}'
-    return unique_slug
 
 
 class Post(models.Model):
@@ -36,7 +24,7 @@ class Post(models.Model):
         Сохранение полей модели при их отсутствии заполнения
         """
         if not self.slug:
-            self.slug = unique_slugify(self, self.title)
+            self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
     class Meta:
